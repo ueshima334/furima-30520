7556c8482e2e43767774a92c56873f9c9e39145b
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-09 18:32:31 +0900

    pryの導入

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_bot_rails'
   gem 'faker'
+  gem 'pry-rails'
+  gem 'pry-byebug'
+  gem 'pry-doc'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,9 @@ RSpec.describe 'ユーザー新規登録', type: :system do
 
     context '新規登録に失敗する時' do
       it "ニックネームが空欄の時、登録に失敗する" do
-        @user.email = ""
+        @user.nickname = ""
+        @user
+        expect(@user.errors.full_messages).to include("NickName can't be blank")
       end
 
       it "メールアドレスが空欄の時、登録に失敗する" do
