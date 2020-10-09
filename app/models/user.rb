008c555f45ea57be3880fd66c9387fe26b937981
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-09 15:28:00 +0900

    エラーメッセージ表示の設定、それに伴うバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,        presence: true
-         validates :last_name,       presence: true
-         validates :first_name,      presence: true
-         validates :last_name_kana,  presence: true
-         validates :first_name_kana, presence: true
-         validates :birthday,        presence: true
+         validates :nickname,        presence: { message: "名前を入力してください" }
+         validates :birthday,        presence: { message: "生年月日を入力してください" }
+         validates :password,        presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください' }
+
+         with_options presence: { message: "名前を入力してください" }, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+          validates :last_name
+          validates :first_name
+          validates :last_name_kana
+          validates :first_name_kana
+        end
 end
