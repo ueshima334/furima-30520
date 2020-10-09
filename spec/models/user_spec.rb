9a8bdb98892c5a01c83c003664e769d0aa0f42e0
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-09 18:20:08 +0900

    factorybotの読み込みエラーの為、修正

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+RSpec.describe 'ユーザー新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録に成功する時' do
+      it "入力項目が全て正しく入力された時、登録に成功する" do
+        @user.valid?
+      end
+    end
+
+    context '新規登録に失敗する時' do
+      it "ニックネームが空欄の時、登録に失敗する" do
+        @user.email = ""
+      end
+
+      it "メールアドレスが空欄の時、登録に失敗する" do
+      end
+      
+    
+    end
+
+  end
+end
+
