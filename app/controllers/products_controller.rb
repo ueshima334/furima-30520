2f1d73175cc4a9e43b5b4e891da51d3e369c2042
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-10 16:23:44 +0900

    createアクションの処理内容を記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,20 @@ class ProductsController < ApplicationController
   end
 
   def create
+    product = Product.new(product_params)
+    if product.save
+      redirect_to root_path
+    else
+      @product = Product.new
+      render :new
+    end
   end
+
+private
+
+  def product_params
+    params.require(:product).permit(:title,:details,:category_id,:status_id,:delivery_charge_id,:area_id,:days_id,:price,:image).merge(user_id:current_user.id)
+  end
+
 
 end
