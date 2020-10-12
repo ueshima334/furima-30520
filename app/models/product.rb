beb6c350f969a3202153f344319c565c37e1b2c1
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-12 18:32:42 +0900

    バリデーションの記述を纏めた

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,16 +9,23 @@ class Product < ApplicationRecord
   belongs_to_active_hash :days
   has_one_attached :image
 
-  validates :title,              presence: { message: 'を入力してください' }
-  validates :details,            presence: { message: 'を入力してください' }
-  validates :category_id,        numericality: { other_than: 1,message: 'を入力してください' }
-  validates :status_id,          numericality: { other_than: 1,message: 'を入力してください' }
-  validates :delivery_charge_id, numericality: { other_than: 1,message: 'を入力してください' }
-  validates :area_id,            numericality: { other_than: 1,message: 'を入力してください' }
-  validates :days_id,            numericality: { other_than: 1,message: 'を入力してください' }
-  validates :price,              numericality: { message: 'は半角数字で入力してください' }
-  validates :price,              presence: { message: 'を入力してください'},
-    numericality: { greater_than_or_equal_to: 300, message: 'は300~9999999の範囲で指定してください',less_than_or_equal_to: 9999999, message: 'は300~9999999の範囲で指定してください'}
-  validates :image,              presence: { message: 'を選択してください'}
+  with_options numericality: { other_than: 1,message: 'を入力してください' } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :area_id
+    validates :days_id
+  end
+
+  with_options presence: { message: 'を入力してください' } do
+    validates :title
+    validates :details 
+    validates :price
+  end
+
+  validates :image,   presence: { message: 'を選択してください'}
+  validates :price,   numericality: { message: 'は半角数字で入力してください' },allow_blank: true
+  validates :price,   numericality: { greater_than_or_equal_to: 300, message: 'は300~9999999の範囲で指定してください',
+                      less_than_or_equal_to: 9999999, message: 'は300~9999999の範囲で指定してください'},allow_blank: true
 
 end
