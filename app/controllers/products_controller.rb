3ba0b2a9d14c75fc92e40c3a32573cfea1127c2a
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-13 14:15:14 +0900

    編集機能におけるルーティングとアクションを定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,12 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
 
   def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 root to: "products#index"
-resources :products,only: [:new,:create,:show]
+resources :products,only: [:new,:create,:show,:edit,:update]
 
 end
