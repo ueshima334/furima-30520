407035d3eaaab22dff93427bd18a3afd1e76fd03
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-14 11:56:41 +0900

    出品者とログアウトユーザーが購入画面にアクセスできないように記述

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,5 +1,12 @@
 class BuyersController < ApplicationController
+
   def index
     @product = Product.find(params[:product_id])
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    elsif user_signed_in? && current_user.id == @product.user_id
+      redirect_to controller: :products, action: :index
+    end
   end
+
 end
