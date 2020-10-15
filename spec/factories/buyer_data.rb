ea480cc9a558d626c0acffd4017c5031581d3182
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-15 21:11:22 +0900

    buyerdataモデルの単体テストコードを記述

--- spec/factories/buyer_data.rb
+++ spec/factories/buyer_data.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
-  factory :buyer_datum do
-    
+  factory :BuyerData do
+    postal_code    { '111-1111' }
+    prefecture_id  { 2 }
+    city           { '横浜市緑区' }
+    address        { '青山1-1-1' }
+    phone_number   { 1111111111 }
+    token {"tok_abcdefghijk00000000000000000"}
+
+    association :buyer
   end
 end
